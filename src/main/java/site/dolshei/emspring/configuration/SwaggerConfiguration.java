1e234447fff6fa287779894ce79d35e89e519696
Dolshei <dolshei@gmail.com>
2022-12-24 10:31:02 +0900

    Swagger 설정 수정

--- src/main/java/site/dolshei/emspring/configuration/SwaggerConfiguration.java
+++ src/main/java/site/dolshei/emspring/configuration/SwaggerConfiguration.java
@@ -7,9 +7,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 //@EnableSwagger2
@@ -17,18 +15,18 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("site.dolshei.emspring.mvc.controller"))
+                .apis(RequestHandlerSelectors.basePackage("site.dolshei.emspring.mvc"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+                .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("API 서버 문서")
-                .description("API 서버 문서 입니다.")
+                .description("API 문서 설명")
+                .version("1.0.0")
                 .build();
     }
 /*
